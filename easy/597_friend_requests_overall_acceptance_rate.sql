7cbde9063514286e1bbdab08be5d34a8114052a9
Demodemo <jiangtaotao2@gmail.com>
2022-02-17 21:53:17 -0500

    Solve follow up question

--- easy/597_friend_requests_overall_acceptance_rate.sql
+++ easy/597_friend_requests_overall_acceptance_rate.sql
@@ -3,3 +3,22 @@ select
 from
 	FriendRequest, RequestAccepted
 ;
+
+# follow up question 1
+select 
+	if(b.req=0, 0.00, round(a.cnt/b.cnt, 2) as accept_rate, a.month)
+from
+	(select 
+		count(distinct requeter_id, accepter_id) as cnt, Month(accept_date)	as month 
+	from 
+		request_accepted
+	) as a,
+	(
+	select 
+		count (distinct sender_id, sent_to_id) as cnt, Month(request_date) as month
+	from
+		friend_request
+	) as b
+where a.month = b.month
+group by a.month
+;
