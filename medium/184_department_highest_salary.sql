e8ed908546ff0df7f68eb5632ec5accdc46e6583
Demodemo <jiangtaotao2@gmail.com>
2022-06-29 21:36:25 -0400

    Use of common expression table

--- medium/184_department_highest_salary.sql
+++ medium/184_department_highest_salary.sql
@@ -9,3 +9,18 @@ on a.department = b.id
 ) as c
 where c.r = 1
 ;
+
+# Use CTE - common expression table
+with salary_rank_dept as (
+select b.name as department, a.name as employee, a.salary,
+    rank() over(partition by a.departmentid order by a.salary desc) as salary_rank
+from employee as a join department as b
+on a.departmentid = b.id
+        )
+
+select
+    department, employee, salary
+from
+    salary_rank_dept
+where salary_rank = 1
+;
